8fe348b4575b772004fd4b146ff512f904d84556
Vladislav Andreev <andreev@vlad.social>
2023-09-29 09:42:12 +0300

    Add model validations

--- app/models/excavator.rb
+++ app/models/excavator.rb
@@ -1,5 +1,6 @@
 class Excavator < ApplicationRecord
   belongs_to :ticket
+  validates_presence_of :ticket, :company_name, :address
 
   def self.create_with_attrs(ticket:, **attrs)
     concat_address = [attrs[:address], attrs[:city], attrs[:state], attrs[:zip]].join(", ")
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,6 @@
 class Ticket < ApplicationRecord
-  has_one :excavator
+  has_one :excavator, dependent: :destroy
+  validates_presence_of :excavator, :request_number, :sequence_number, :request_type,
+    :request_action, :response_due_date_time, :digsite_info, :additional_service_area_codes,
+    :primary_service_area_code
 end
