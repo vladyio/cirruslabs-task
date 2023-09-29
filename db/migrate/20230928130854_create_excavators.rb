ebbd2757561c0ff9948d4d0d0bbec9a96886d9d3
Vladislav Andreev <andreev@vlad.social>
2023-09-29 09:56:54 +0300

    Fix cascade deletion of excavators

--- db/migrate/20230928130854_create_excavators.rb
+++ db/migrate/20230928130854_create_excavators.rb
@@ -1,7 +1,7 @@
 class CreateExcavators < ActiveRecord::Migration[7.0]
   def change
     create_table :excavators do |t|
-      t.references :ticket, foreign_key: true
+      t.references :ticket, foreign_key: true, on_delete: :cascade
       t.string :company_name, null: false
       t.string :address, null: false
 
